793d053309559f85b1dedf72b34a219b2c6f34f0
ridzuan-ansi <ridzuan@ansi.com.my>
2021-07-17 20:37:10 +0800

    add detection script

--- GPS Location/Assets/Scripts/GPS.cs
+++ GPS Location/Assets/Scripts/GPS.cs
@@ -3,23 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Globalization;
+using System;
 
 public class GPS : MonoBehaviour
 {
-    public float Latitude;
-    public float Longitude;
+    private float Latitude;
+    private float Longitude;
+
+    private bool is_start_routine;
+    private String status;
 
     public Text lbl_location;
+    public Text lbl_updated_at;
+    public Text lbl_service_status;
+     public Text lbl_routine_status;
 
     // Start is called before the first frame update
-    IEnumerator Start()
+    void Start()
     {
+        Input.compass.enabled = true;
+        StartCoroutine(getLocation());
+    }
+
+    IEnumerator getLocation()
+    {
+        is_start_routine = true;
+
         // First, check if user has location service enabled
         if (!Input.location.isEnabledByUser)
+        {
+            status = "location service not enabled";
             yield break;
-
+        }
+        
         // Start service before querying location
-        Input.location.Start();
+        Input.location.Start(1,1);
 
         // Wait until service initializes
         int maxWait = 20;
@@ -32,21 +50,27 @@ public class GPS : MonoBehaviour
         // Service didn't initialize in 20 seconds
         if (maxWait < 1)
         {
-            print("Timed out");
+            status = "Timed out";
             yield break;
         }
 
         // Connection has failed
         if (Input.location.status == LocationServiceStatus.Failed)
         {
-            print("Unable to determine device location");
+            status = "Unable to determine device location";
             yield break;
         }
         else
         {
+            Latitude = Input.location.lastData.latitude;
+            Longitude = Input.location.lastData.longitude;
+            lbl_location.text = "Latitude : " + Latitude + " , Longitude : " + Longitude;
             // Access granted and location value could be retrieved
-            print("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+            // print("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
         }
+
+        status = "ready..";
+        is_start_routine = false;
 
         // Stop service if there is no need to query location updates continuously
         //Input.location.Stop();
@@ -55,16 +79,12 @@ public class GPS : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.location.status == LocationServiceStatus.Running)
-        {
-            Latitude = Input.location.lastData.latitude;
-            Longitude = Input.location.lastData.longitude;
+        Latitude = Input.location.lastData.latitude;
+        Longitude = Input.location.lastData.longitude;
 
-            lbl_location.text = "Lat : " + Latitude + " , Lng : " + Longitude;
-        }
-        else
-        {
-            lbl_location.text = "GPS services not available";
-        }
+        lbl_routine_status.text = is_start_routine.ToString();
+        lbl_service_status.text = Input.location.status.ToString() + "[" + status + "]";
+        lbl_location.text = "Lat : " + Latitude + " , Lng : " + Longitude;
+        lbl_updated_at.text = DateTime.Now.ToString("dd/mm/yyyy HH:mm:sss");
     }
 }
